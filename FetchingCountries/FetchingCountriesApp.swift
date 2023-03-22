97d5e0c4e5375aed4dc758d778d825126475ff6b
Jacob M Sock <jacobmsock@gmail.com>
2023-03-22 13:38:08 -0400

    Initial Commit

---
+++ FetchingCountries/FetchingCountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchingCountriesApp.swift
+//  FetchingCountries
+//
+//  Created by Jacob Sock on 3/22/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchingCountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
